4f23233d8b08cbe757b2f13b7a6478cfb643c2b0
simhani1 <simhani1@gmail.com>
2023-01-23 16:54:00 +0900

    [2-3,4] 엔티티 클래스 개발1, 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,15 +1,27 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String address;
+
+
+    /**
+     * JPA 구현 라이브러리가 객체를 생성할 때 리플렉션 같은 기술을 사용할 수 있도록 지원해야 하기 때문이다.
+     *  */
+    protected Address() {  // jpa에서 스펙상 만들어 줘야 한다.
+    }
+
+    public Address(String city, String street, String address) {
+        this.city = city;
+        this.street = street;
+        this.address = address;
+    }
 }
