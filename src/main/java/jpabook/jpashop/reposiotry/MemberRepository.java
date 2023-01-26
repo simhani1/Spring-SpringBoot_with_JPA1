65cb8b6a6ca306adf9cdfdca250964ee676622dc
simhani1 <simhani1@gmail.com>
2023-01-27 01:57:27 +0900

    [3-2] 회원 서비스 개발

--- src/main/java/jpabook/jpashop/reposiotry/MemberRepository.java
+++ src/main/java/jpabook/jpashop/reposiotry/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.reposiotry;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,9 +9,13 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor  // 스프링 부트에서 entitymanger를 autowired로 주입할 수 있는 기능을 제공해준다.
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+
+//    @PersistenceContext
+//    private EntityManager em;
+
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
