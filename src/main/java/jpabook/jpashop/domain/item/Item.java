91eb1bacf56174b2645af04c60c074c603399688
simhani1 <simhani1@gmail.com>
2023-01-27 14:22:57 +0900

    [4-1]  상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    /**
+     * 데이터에 변경이 필요한 경우 setter를 사용하지 않고 메서드를 통해 조작하는 것이 객체지향적이다.
+     * */
+
+    //==비즈니스 로직==//
+    // 재고 증가
+    public void addStock(int quantity) {
+        this.stokQuantity = quantity;
+    }
+
+    //==비즈니스 로직==//
+    // 재고 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stokQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stokQuantity = restStock;
+    }
+
 }
