f9fb935a3e9b67f809c6dfd5fc59b62773222ea7
simhani1 <simhani1@gmail.com>
2023-01-27 14:27:15 +0900

    [4-2] 상품 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/reposiotry/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.reposiotry;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist((item));  // 실제 추가
+        } else {
+            em.merge(item);  // 업데이트라고 생각을 하자
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}
