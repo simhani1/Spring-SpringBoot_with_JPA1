bc7f46810270135d089f90add4fd8dcc1440600a
simhani1 <simhani1@gmail.com>
2023-01-28 22:28:45 +0900

    [6-3] 회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -7,13 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.naming.Binding;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -45,5 +43,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,7 +10,7 @@ public class Address {
 
     private String city;
     private String street;
-    private String address;
+    private String zipcode;
 
 
     /**
@@ -19,9 +19,9 @@ public class Address {
     protected Address() {  // jpa에서 스펙상 만들어 줘야 한다.
     }
 
-    public Address(String city, String street, String address) {
+    public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
-        this.address = address;
+        this.zipcode = zipcode;
     }
 }
